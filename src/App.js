8804a378dbf5eb08bdfb98d89cbf4132b5a0db48
tos4christ <gnztrades@gmail.com>
2022-07-15 10:04:05 +0100

    Added Signup and Signin Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Link, Switch } from 'react-router-dom';
 import './App.css';
 import './components/css/Header.css';
 import './components/css/table.css';
 import Home from './components/Home';
 import Header from './components/Header';
+import SignUp from '../src/components/Users/SignUp';
+import SignIn from '../src/components/Users/SignIn';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
+  setUserDetails() {
 
+  }
   render() {
     return (
       <Router>
-        <div className="App">
-          <Header />
-          <Home />
-        </div>
+        <Switch >
+          <Route path={'/'}>
+            <SignIn setUser={this.setUserDetails} />
+          </Route>
+          <Route path={'/signin'}>
+            <SignIn setUser={this.setUserDetails} />
+          </Route>
+          <Route path={'/signup'}>
+            <SignUp />
+          </Route>          
+          {/* This is the protected path after successful login */}
+          <Route path={'/home'}>
+            <div className="App">
+              <Header />
+              <Home />
+            </div>
+          </Route>
+        </Switch>
       </Router>      
     )    
   };
---
+++ src/components/Users/SignIn.js
@@ -0,0 +1,90 @@
+import React from "react";
+import { withRouter } from 'react-router-dom';
+import Text from "../components/Inputs/Text";
+import Button from "../components/Inputs/Button";
+import Link from "../components/Inputs/Links";
+// import socket from "../utility/socketioConnection";
+
+class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+    this.setEmail = this.setEmail.bind(this);
+    this.setPassword = this.setPassword.bind(this);
+    this.state = {
+      email: '',
+      password: ''
+    }
+  }
+  setEmail(email) {
+    this.setState({email:email})
+  }
+  setPassword(password) {
+    this.setState({password: password})
+  }
+  handleSubmission = (e) => {
+    e.preventDefault();
+    const url = "/signin";
+    const email = this.state.email;
+    const password = this.state.password;
+    if (email === "" || password === "") {
+      return;
+    }
+    const data = { email, password };
+    fetch(url, {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((res) => res.json())
+    .then((response) => {
+      // check the response to see if this is their first login attempt
+      const firstLogin = response.status;
+
+      // If first login then redirect to change password page
+      this.props.history.push({pathname: `/changePassword`});
+      
+      //If this is not the first login Redirect to home page
+      this.props.history.push({pathname: `/home`});
+    })
+    .catch((error) => console.error(error.message));
+  };
+  
+  render() {
+    return (
+      <div className="py-4 responders-bg container-fluid bg-light">
+        <div className="row mt-4">
+          <div className="col-sm-4 mx-auto mt-4 pt-4 bg-white shadow">
+            <div className="login-bg"></div>
+            <form className="mt-3" onSubmit={this.handleSubmission} autoComplete="on">
+              <Text
+                type={"email"}
+                placeholder={"User Email"}
+                name={"email"}
+                icon={"fa fa-envelope"}
+                nameChange={ this.setEmail }
+              />
+              <Text
+                type={"password"}
+                placeholder={"Password"}
+                name={"password"}
+                icon={"fa fa-lock"}
+                nameChange={ this.setPassword }
+              />
+              <Link
+                question="Not Registered? "
+                link="Sign Up"
+                linkTo="/signup"
+              />
+              <Button id="" text={"Login"} onClick={() => "coming"} />
+            </form>
+          </div>
+        </div>
+      </div>
+    );
+  }  
+};
+
+export default withRouter(SignIn);
