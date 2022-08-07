e459e272bfe7020b40a5cd085615c746765b7281
tos4christ <gnztrades@gmail.com>
2022-08-07 10:44:52 +0100

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -26,7 +26,7 @@ class Header extends React.Component {
     }
     const { value } = this.state; 
     return (      
-        <nav className='header navbar-expand-md justify-content-center navbar-inverse'>
+        <nav className='header navbar-expand-sm navbar-inverse'>
             <div className='container-fluid'>
                 <div className='navbar-header'>
                     <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
