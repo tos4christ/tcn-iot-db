0b7ebc416bf9348bcadd302dcc8bb5ede05f4b27
tos4christ <gnztrades@gmail.com>
2022-08-13 12:42:03 +0100

    added Protected route for the home login component

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Home from './components/Home';
 import Header from './components/Header';
 import SignUp from '../src/components/Users/SignUp';
 import SignIn from '../src/components/Users/SignIn';
-import Protected from './components/Protected';
+// import Protected from './components/Protected';
 
 class App extends React.Component {
   setUserDetails() {
@@ -36,18 +36,12 @@ class App extends React.Component {
             </div>
           </Route>          
           {/* This is the protected path after successful login */}
-          <Route 
-          path={'/home'}
-          element={
-            <Protected isLoggedIn={localStorage.getItem("isLoggedIn")}>
-              <div className="App">
-                <Header />
-                <Home />
-              </div>
-            </Protected>
-          }
-          
-          />
+          <Route path={'/home'} >
+            <div className="App">
+              <Header />
+              <Home />
+            </div>
+          </Route>
           <Route path={'/'}>
             <div className='App'>              
               <Header />
