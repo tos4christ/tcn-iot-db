83dbb0ea5d76a0f6204e355d6923793d0e963ecf
tos4christ <gnztrades@gmail.com>
2022-08-24 21:05:07 +0100

    completed the station adder logic

--- src/components/stations_adder.js
+++ src/components/stations_adder.js
@@ -102,131 +102,131 @@ export default (state_data) => {
     
     const station_array = { 
         'EKET': {mw: Number(
-            (eket_e21m.td.mw ? eket_e21m.td.mw : 0) + (eket_e22m.td.mw ? eket_e22m.td.mw : 0)
-            ).toFixed(2), kv: (eket_e21m.td.V ? eket_e21m.td.V : eket_e22m.td.V ? eket_e22m.td.V : 0)},
+            (eket_e21m.td ? eket_e21m.td.mw : 0) + (eket_e22m.td ? eket_e22m.td.mw : 0)
+            ).toFixed(2), kv: (eket_e21m.td ? eket_e21m.td.V : eket_e22m.td ? eket_e22m.td.V : 0)},
         'PORT-HARCOURT MAIN' : {mw: Number(
-            (phMain_m21p.td.mw ? phMain_m21p.td.mw : 0)
-            ).toFixed(2), kv: (phMain_m21p.td.V ? phMain_m21p.td.V : 0)},
+            (phMain_m21p.td ? phMain_m21p.td.mw : 0)
+            ).toFixed(2), kv: (phMain_m21p.td ? phMain_m21p.td.V : 0)},
         'LOKOJA TS' : {mw: Number(
-            ((lokojaTs_j1l.td.mw ? lokojaTs_j1l.td.mw : 0) + (lokojaTs_j2l.td.mw ? lokojaTs_j2l.td.mw : 0)) - 
-            ((lokojaTs_l6g.td.mw ? lokojaTs_l6g.td.mw : 0) + (lokojaTs_l7g.td.mw ? lokojaTs_l7g.td.mw : 0))
-                ).toFixed(2), kv: (lokojaTs_j1l.td.V ? lokojaTs_j1l.td.V : 0)},
+            ((lokojaTs_j1l.td ? lokojaTs_j1l.td.mw : 0) + (lokojaTs_j2l.td ? lokojaTs_j2l.td.mw : 0)) - 
+            ((lokojaTs_l6g.td ? lokojaTs_l6g.td.mw : 0) + (lokojaTs_l7g.td ? lokojaTs_l7g.td.mw : 0))
+                ).toFixed(2), kv: (lokojaTs_j1l.td ? lokojaTs_j1l.td.V : 0)},
         'EKIM' : {mw: Number(
-            (ekim_ek1m.td.mw ? ekim_ek1m.td.mw : 0)
-            ).toFixed(2), kv: ekim_ek1m.td.V ? ekim_ek1m.td.V : 0},
+            (ekim_ek1m.td ? ekim_ek1m.td.mw : 0)
+            ).toFixed(2), kv: ekim_ek1m.td ? ekim_ek1m.td.V : 0},
         'IKOT EKPENE' : {mw: Number(
-            ((ikotEkpene_a1k.td.mw?ikotEkpene_a1k.td.mw:0) + (ikotEkpene_a2k.td.mw?ikotEkpene_a2k.td.mw:0) +
-            (ikotEkpene_d1k.td.mw?ikotEkpene_d1k.td.mw:0) + (ikotEkpene_d2k.td.mw?ikotEkpene_d2k.td.mw:0)) - 
-            ((ikotEkpene_k1u.td.mw?ikotEkpene_k1u.td.mw:0) + (ikotEkpene_k2u.td.mw?ikotEkpene_k2u.td.mw:0) + 
-            (ikotEkpene_k3u.td.mw?ikotEkpene_k3u.td.mw:0) + (ikotEkpene_k4u.td.mw?ikotEkpene_k4u.td.mw:0))
-                ).toFixed(2), kv: ikotEkpene_d1k.td.V?ikotEkpene_d1k.td.V:0}, 
+            ((ikotEkpene_a1k.td ? ikotEkpene_a1k.td.mw:0) + (ikotEkpene_a2k.td ?ikotEkpene_a2k.td.mw:0) +
+            (ikotEkpene_d1k.td ?ikotEkpene_d1k.td.mw:0) + (ikotEkpene_d2k.td ?ikotEkpene_d2k.td.mw:0)) - 
+            ((ikotEkpene_k1u.td ?ikotEkpene_k1u.td.mw:0) + (ikotEkpene_k2u.td ?ikotEkpene_k2u.td.mw:0) + 
+            (ikotEkpene_k3u.td ?ikotEkpene_k3u.td.mw:0) + (ikotEkpene_k4u.td ?ikotEkpene_k4u.td.mw:0))
+                ).toFixed(2), kv: ikotEkpene_d1k.td ?ikotEkpene_d1k.td.V:0}, 
         'GWAGWALADA' : {mw: Number(
-            (gwagwalada_g5b.td.mw?gwagwalada_g5b.td.mw:0) + (gwagwalada_r3g.td.mw? gwagwalada_r3g.td.mw:0)
-            ).toFixed(2), kv: (gwagwalada_g5b.td.V?gwagwalada_g5b.td.V:0)},
+            (gwagwalada_g5b.td ?gwagwalada_g5b.td.mw:0) + (gwagwalada_r3g.td ? gwagwalada_r3g.td.mw:0)
+            ).toFixed(2), kv: (gwagwalada_g5b.td ?gwagwalada_g5b.td.V:0)},
         'UGWUAJI' : {mw: Number(
-            (ugwuaji_u1a.td.mw?ugwuaji_u1a.td.mw:0) + (ugwuaji_u2a.td.mw?ugwuaji_u2a.td.mw:0)
-            ).toFixed(2), kv: ugwuaji_u1a.td.V?ugwuaji_u1a.td.V:0},
+            (ugwuaji_u1a.td ?ugwuaji_u1a.td.mw:0) + (ugwuaji_u2a.td ?ugwuaji_u2a.td.mw:0)
+            ).toFixed(2), kv: ugwuaji_u1a.td ?ugwuaji_u1a.td.V:0},
         'ASABA' : {mw: Number(
-            (asaba_b3d.td.mw?asaba_b3d.td.mw:0) + (asaba_d3t.td.mw?asaba_d3t.td.mw:0)
-            ).toFixed(2), kv: (asaba_b3d.td.V?asaba_b3d.td.V:0)},
+            (asaba_b3d.td ?asaba_b3d.td.mw:0) + (asaba_d3t.td ?asaba_d3t.td.mw:0)
+            ).toFixed(2), kv: (asaba_b3d.td ?asaba_b3d.td.V:0)},
         'SHIRORO (HYDRO)' : {mw: Number(
-            (shiroroPs_411g1.gd.mw?shiroroPs_411g1.gd.mw:0) + (shiroroPs_411g2.gd.mw?shiroroPs_411g2.gd.mw:0) + 
-            (shiroroPs_411g3.gd.mw?shiroroPs_411g3.gd.mw:0) + (shiroroPs_411g4.gd.mw?shiroroPs_411g4.gd.mw:0)  
-                ).toFixed(2), kv: (shiroroPs_411g1.gd.V?shiroroPs_411g1.gd.V:0)},
+            (shiroroPs_411g1.gd ?shiroroPs_411g1.gd.mw:0) + (shiroroPs_411g2.gd ?shiroroPs_411g2.gd.mw:0) + 
+            (shiroroPs_411g3.gd ?shiroroPs_411g3.gd.mw:0) + (shiroroPs_411g4.gd ?shiroroPs_411g4.gd.mw:0)  
+                ).toFixed(2), kv: (shiroroPs_411g1.gd ?shiroroPs_411g1.gd.V:0)},
         'AFAM IV & V (GAS)' : {mw: Number(
-            (afamIv_vPs_gt17.td.mw?afamIv_vPs_gt17.td.mw:0) + (afamIv_vPs_gt18.td.mw?afamIv_vPs_gt18.td.mw:0)
-            ).toFixed(2), kv: (afamIv_vPs_gt17.td.V?afamIv_vPs_gt17.td.V:0)},
+            (afamIv_vPs_gt17.td ?afamIv_vPs_gt17.td.mw:0) + (afamIv_vPs_gt18.td ?afamIv_vPs_gt18.td.mw:0)
+            ).toFixed(2), kv: (afamIv_vPs_gt17.td ?afamIv_vPs_gt17.td.V:0)},
         'KAINJI (HYDRO)' : {mw: Number(
-            (kainjiTs_k1f.td.mw?kainjiTs_k1f.td.mw:0) + (kainjiTs_k1j.td.mw?kainjiTs_k1j.td.mw:0) + 
-            (kainjiTs_k2j.td.mw?kainjiTs_k2j.td.mw:0) + (kainjiTs_k3r.td.mw?kainjiTs_k3r.td.mw:0)
-                ).toFixed(2), kv: (kainjiTs_k1f.td.V?kainjiTs_k1f.td.V:0)},
+            (kainjiTs_k1f.td ?kainjiTs_k1f.td.mw:0) + (kainjiTs_k1j.td ?kainjiTs_k1j.td.mw:0) + 
+            (kainjiTs_k2j.td ?kainjiTs_k2j.td.mw:0) + (kainjiTs_k3r.td ?kainjiTs_k3r.td.mw:0)
+                ).toFixed(2), kv: (kainjiTs_k1f.td ?kainjiTs_k1f.td.V:0)},
         'EGBIN (STEAM)' : {mw: Number(
-            (egbinPs_st1.gd.mw?egbinPs_st1.gd.mw:0) + (egbinPs_st2.gd.mw?egbinPs_st2.gd.mw:0) + 
-            (egbinPs_st3.gd.mw?egbinPs_st3.gd.mw:0) + (egbinPs_st4.gd.mw?egbinPs_st4.gd.mw:0) + 
-            (egbinPs_st5.gd.mw?egbinPs_st5.gd.mw:0) + (egbinPs_st6.gd.mw?egbinPs_st6.gd.mw:0)
-                ).toFixed(2), kv: (egbinPs_st1.gd.V?egbinPs_st1.gd.V:0)},
+            (egbinPs_st1.gd ?egbinPs_st1.gd.mw:0) + (egbinPs_st2.gd ?egbinPs_st2.gd.mw:0) + 
+            (egbinPs_st3.gd ?egbinPs_st3.gd.mw:0) + (egbinPs_st4.gd ?egbinPs_st4.gd.mw:0) + 
+            (egbinPs_st5.gd ?egbinPs_st5.gd.mw:0) + (egbinPs_st6.gd ?egbinPs_st6.gd.mw:0)
+                ).toFixed(2), kv: (egbinPs_st1.gd ?egbinPs_st1.gd.V:0)},
         'OKPAI (GAS/STEAM)' : {mw: Number(
-            (okpaiGs_k1t.td.mw?okpaiGs_k1t.td.mw:0) + (okpaiGs_k2t.td.mw?okpaiGs_k2t.td.mw:0)
-            ).toFixed(2), kv: (okpaiGs_k1t.td.V?okpaiGs_k1t.td.V:0)},
+            (okpaiGs_k1t.td ?okpaiGs_k1t.td.mw:0) + (okpaiGs_k2t.td ?okpaiGs_k2t.td.mw:0)
+            ).toFixed(2), kv: (okpaiGs_k1t.td ?okpaiGs_k1t.td.V:0)},
         'DELTA (GAS)' : {mw: Number(
-            (delta2_tr3.gd.mw?delta2_tr3.gd.mw:0) + (delta2_tr4.gd.mw?delta2_tr4.gd.mw:0) + 
-            (delta3_tr5.gd.mw?delta3_tr5.gd.mw:0) + (delta3_tr6.gd.mw?delta3_tr6.gd.mw:0) + 
-            (deltaGs_g3b.td.mw?deltaGs_g3b.td.mw:0) + (deltaGs_s4g.td.mw?deltaGs_s4g.td.mw:0)   
-                ).toFixed(2), kv: (deltaGs_g3b.td.V?deltaGs_g3b.td.V:0)},
+            (delta2_tr3.gd ?delta2_tr3.gd.mw:0) + (delta2_tr4.gd ?delta2_tr4.gd.mw:0) + 
+            (delta3_tr5.gd ?delta3_tr5.gd.mw:0) + (delta3_tr6.gd ?delta3_tr6.gd.mw:0) + 
+            (deltaGs_g3b.td ?deltaGs_g3b.td.mw:0) + (deltaGs_s4g.td ?deltaGs_s4g.td.mw:0)   
+                ).toFixed(2), kv: (deltaGs_g3b.td ?deltaGs_g3b.td.V:0)},
         'JEBBA (HYDRO)' : {mw: Number(
-            (jebbaTs_b8j.td.mw?jebbaTs_b8j.td.mw:0) + (jebbaTs_b9j.td.mw?jebbaTs_b9j.td.mw:0)
-            ).toFixed(2), kv: (jebbaTs_b8j.td.V?jebbaTs_b8j.td.V:0)},
+            (jebbaTs_b8j.td ?jebbaTs_b8j.td.mw:0) + (jebbaTs_b9j.td ?jebbaTs_b9j.td.mw:0)
+            ).toFixed(2), kv: (jebbaTs_b8j.td ?jebbaTs_b8j.td.V:0)},
         'AFAM VI (GAS/STEAM)' : {mw: Number(
-            (afamViTs_ada200.td.mw?afamViTs_ada200.td.mw:0) + (afamViTs_adb200.td.mw?afamViTs_adb200.td.mw:0)
-            ).toFixed(2), kv: (afamViTs_ada200.td.V?afamViTs_ada200.td.V:0)},
+            (afamViTs_ada200.td ?afamViTs_ada200.td.mw:0) + (afamViTs_adb200.td ?afamViTs_adb200.td.mw:0)
+            ).toFixed(2), kv: (afamViTs_ada200.td ?afamViTs_ada200.td.V:0)},
         'ALAOJI NIPP (GAS)' : {mw: Number(
-            (alaoji_l7a.td.mw?alaoji_l7a.td.mw:0) + (alaoji_l8a.td.mw?alaoji_l8a.td.mw:0)
-            ).toFixed(2), kv: (alaoji_l7a.td.V?alaoji_l7a.td.V:0)},
+            (alaoji_l7a.td ?alaoji_l7a.td.mw:0) + (alaoji_l8a.td ?alaoji_l8a.td.mw:0)
+            ).toFixed(2), kv: (alaoji_l7a.td ?alaoji_l7a.td.V:0)},
         'SAPELE (STEAM)' : {mw: Number(
-            (sapeleNippPs_st1.gd.mw?sapeleNippPs_st1.gd.mw:0) + (sapeleNippPs_st3.gd.mw?sapeleNippPs_st3.gd.mw:0)
-            ).toFixed(2), kv: sapeleNippPs_st1.gd.V?sapeleNippPs_st1.gd.V:0},
+            (sapeleNippPs_st1.gd ?sapeleNippPs_st1.gd.mw:0) + (sapeleNippPs_st3.gd ?sapeleNippPs_st3.gd.mw:0)
+            ).toFixed(2), kv: sapeleNippPs_st1.gd ?sapeleNippPs_st1.gd.V:0},
         'SAPELE NIPP (GAS)' : {mw: Number(
-            (sapeleNippPs_gt1.gd.mw?sapeleNippPs_gt1.gd.mw:0) + (sapeleNippPs_gt2.gd.mw?sapeleNippPs_gt2.gd.mw:0) + 
-            (sapeleNippPs_gt3.gd.mw?sapeleNippPs_gt3.gd.mw:0) + (sapeleNippPs_gt4.gd.mw?sapeleNippPs_gt4.gd.mw:0)
-                ).toFixed(2), kv: (sapeleNippPs_gt1.gd.V?sapeleNippPs_gt1.gd.V:0)},
+            (sapeleNippPs_gt1.gd ?sapeleNippPs_gt1.gd.mw:0) + (sapeleNippPs_gt2.gd ?sapeleNippPs_gt2.gd.mw:0) + 
+            (sapeleNippPs_gt3.gd ?sapeleNippPs_gt3.gd.mw:0) + (sapeleNippPs_gt4.gd ?sapeleNippPs_gt4.gd.mw:0)
+                ).toFixed(2), kv: (sapeleNippPs_gt1.gd ?sapeleNippPs_gt1.gd.V:0)},
         'ODUKPANI NIPP (GAS)' : {mw: Number(
-            (odukpaniGs_d1b.td.mw?odukpaniGs_d1b.td.mw:0) + (odukpaniGs_d2b.td.mw?odukpaniGs_d2b.td.mw:0) + 
-            (ikotEkpene_d1k.td.mw?ikotEkpene_d1k.td.mw:0) + (ikotEkpene_d2k.td.mw?ikotEkpene_d2k.td.mw:0)
-                ).toFixed(2), kv: (odukpaniGs_d1b.td.V?odukpaniGs_d1b.td.V:0)},
+            (odukpaniGs_d1b.td ?odukpaniGs_d1b.td.mw:0) + (odukpaniGs_d2b.td ?odukpaniGs_d2b.td.mw:0) + 
+            (ikotEkpene_d1k.td ?ikotEkpene_d1k.td.mw:0) + (ikotEkpene_d2k.td ?ikotEkpene_d2k.td.mw:0)
+                ).toFixed(2), kv: (odukpaniGs_d1b.td ?odukpaniGs_d1b.td.V:0)},
         'OMOTOSHO (GAS)' : {mw: Number(
-            (omotosho1_tr1.gd.mw?omotosho1_tr1.gd.mw:0) + (omotosho1_tr2.gd.mw?omotosho1_tr2.gd.mw:0) + 
-            (omotosho2_tr3.gd.mw?omotosho2_tr3.gd.mw:0) + omotosho2_tr4.gd.mw?omotosho2_tr4.gd.mw:0
-                ).toFixed(2), kv: (omotosho1_tr1.gd.V?omotosho1_tr1.gd.V:0)},
+            (omotosho1_tr1.gd ?omotosho1_tr1.gd.mw:0) + (omotosho1_tr2.gd ?omotosho1_tr2.gd.mw:0) + 
+            (omotosho2_tr3.gd ?omotosho2_tr3.gd.mw:0) + omotosho2_tr4.gd ?omotosho2_tr4.gd.mw:0
+                ).toFixed(2), kv: (omotosho1_tr1.gd ?omotosho1_tr1.gd.V:0)},
         'GEREGU (GAS)' : {mw: Number(
-            (gereguPs_gt11.gd.mw?gereguPs_gt11.gd.mw:0) + (gereguPs_gt12.gd.mw?gereguPs_gt12.gd.mw:0) + 
-            (gereguPs_gt13.gd.mw?gereguPs_gt13.gd.mw:0)
-                ).toFixed(2), kv: (gereguPs_gt11.gd.V?gereguPs_gt11.gd.V:0)},
+            (gereguPs_gt11.gd ?gereguPs_gt11.gd.mw:0) + (gereguPs_gt12.gd ?gereguPs_gt12.gd.mw:0) + 
+            (gereguPs_gt13.gd ?gereguPs_gt13.gd.mw:0)
+                ).toFixed(2), kv: (gereguPs_gt11.gd ?gereguPs_gt11.gd.V:0)},
         'RIVERS IPP (GAS)' : {mw: Number(
-            (riversIppPs_gt1.gd.mw?riversIppPs_gt1.gd.mw:0)
-            ).toFixed(2), kv: (riversIppPs_gt1.gd.V?riversIppPs_gt1.gd.V:0)},
+            (riversIppPs_gt1.gd ?riversIppPs_gt1.gd.mw:0)
+            ).toFixed(2), kv: (riversIppPs_gt1.gd ?riversIppPs_gt1.gd.V:0)},
         'OMOKU (GAS)' : {mw: Number(
-            (omokuPs1_o1r.td.mw?omokuPs1_o1r.td.mw:0)
-            ).toFixed(2), kv: (omokuPs1_o1r.td.V?omokuPs1_o1r.td.V:0)},
+            (omokuPs1_o1r.td ?omokuPs1_o1r.td.mw:0)
+            ).toFixed(2), kv: (omokuPs1_o1r.td ?omokuPs1_o1r.td.V:0)},
         'IHOVBOR NIPP (GAS)' : {mw: Number(
-            (ihovborNippPs_gt1.gd.mw?ihovborNippPs_gt1.gd.mw:0) + (ihovborNippPs_gt2.gd.mw?ihovborNippPs_gt2.gd.mw:0) + 
-            (ihovborNippPs_gt3.gd.mw?ihovborNippPs_gt3.gd.mw:0) + (ihovborNippPs_gt4.gd.mw?ihovborNippPs_gt4.gd.mw:0)
-                ).toFixed(2), kv: (ihovborNippPs_gt1.gd.V?ihovborNippPs_gt1.gd.V:0)},
+            (ihovborNippPs_gt1.gd ?ihovborNippPs_gt1.gd.mw:0) + (ihovborNippPs_gt2.gd ?ihovborNippPs_gt2.gd.mw:0) + 
+            (ihovborNippPs_gt3.gd ?ihovborNippPs_gt3.gd.mw:0) + (ihovborNippPs_gt4.gd ?ihovborNippPs_gt4.gd.mw:0)
+                ).toFixed(2), kv: (ihovborNippPs_gt1.gd ?ihovborNippPs_gt1.gd.V:0)},
         'OLORUNSOGO NIPP' : {mw: Number(
-            ((olorunsogoPhase1Gs_r1w.gd.mw?olorunsogoPhase1Gs_r1w.gd.mw:0) + (olorunsogoPhase1Gs_r2a.gd.mw?olorunsogoPhase1Gs_r2a.gd.mw:0)) - 
-            ((olorunsogo1_tr1.gd.mw?olorunsogo1_tr1.gd.mw:0) + (olorunsogo1_tr2.gd.mw?olorunsogo1_tr2.gd.mw:0) + 
-            (olorunsogoPhase1Gs_tr3.gd.mw?olorunsogoPhase1Gs_tr3.gd.mw:0) + (olorunsogoPhase1Gs_tr4.gd.mw?olorunsogoPhase1Gs_tr4.gd.mw:0))
-                ).toFixed(2), kv: (olorunsogoPhase1Gs_r1w.gd.V?olorunsogoPhase1Gs_r1w.gd.V:0)},
+            ((olorunsogoPhase1Gs_r1w.gd ?olorunsogoPhase1Gs_r1w.gd.mw:0) + (olorunsogoPhase1Gs_r2a.gd ?olorunsogoPhase1Gs_r2a.gd.mw:0)) - 
+            ((olorunsogo1_tr1.gd ?olorunsogo1_tr1.gd.mw:0) + (olorunsogo1_tr2.gd ?olorunsogo1_tr2.gd.mw:0) + 
+            (olorunsogoPhase1Gs_tr3.gd ?olorunsogoPhase1Gs_tr3.gd.mw:0) + (olorunsogoPhase1Gs_tr4.gd ?olorunsogoPhase1Gs_tr4.gd.mw:0))
+                ).toFixed(2), kv: (olorunsogoPhase1Gs_r1w.gd ?olorunsogoPhase1Gs_r1w.gd.V:0)},
         'PARAS ENERGY (GAS)' : {mw: Number(
-            (parasEnergyPs_132cb.gd.mw?parasEnergyPs_132cb.gd.mw:0)
-            ).toFixed(2), kv: (parasEnergyPs_132cb.gd.V?parasEnergyPs_132cb.gd.V:0)},
+            (parasEnergyPs_132cb.gd ?parasEnergyPs_132cb.gd.mw:0)
+            ).toFixed(2), kv: (parasEnergyPs_132cb.gd ?parasEnergyPs_132cb.gd.V:0)},
         'OMOTOSHO NIPP (GAS)' : {mw: Number(
-            (omotoshoNippPs_tr1.gd.mw?omotoshoNippPs_tr1.gd.mw:0) + (omotoshoNippPs_tr2.gd.mw?omotoshoNippPs_tr2.gd.mw:0) + 
-            (omotoshoNippPs_tr3.gd.mw?omotoshoNippPs_tr3.gd.mw:0) + (omotoshoNippPs_tr4.gd.mw?omotoshoNippPs_tr4.gd.mw:0)
-                ).toFixed(2), kv: (omotoshoNippPs_tr1.gd.V?omotoshoNippPs_tr1.gd.V:0)},
+            (omotoshoNippPs_tr1.gd ?omotoshoNippPs_tr1.gd.mw:0) + (omotoshoNippPs_tr2.gd ?omotoshoNippPs_tr2.gd.mw:0) + 
+            (omotoshoNippPs_tr3.gd ?omotoshoNippPs_tr3.gd.mw:0) + (omotoshoNippPs_tr4.gd ?omotoshoNippPs_tr4.gd.mw:0)
+                ).toFixed(2), kv: (omotoshoNippPs_tr1.gd ?omotoshoNippPs_tr1.gd.V:0)},
         'GEREGU NIPP (GAS)' : {mw: Number(
-            -((gereguPs_r1j.gd.mw?gereguPs_r1j.gd.mw:0) + (gereguPs_r1j.gd.mw?gereguPs_r1j.gd.mw:0)) - 
-            ((gereguPs_gt11.gd.mw?gereguPs_gt11.gd.mw:0) + (gereguPs_gt12.gd.mw?gereguPs_gt12.gd.mw:0) +
-            (gereguPs_gt13.gd.mw?gereguPs_gt13.gd.mw:0))
-                ).toFixed(2), kv: (gereguPs_r2j.gd.V?gereguPs_r2j.gd.V:0)},
+            -((gereguPs_r1j.gd ?gereguPs_r1j.gd.mw:0) + (gereguPs_r1j.gd ?gereguPs_r1j.gd.mw:0)) - 
+            ((gereguPs_gt11.gd ?gereguPs_gt11.gd.mw:0) + (gereguPs_gt12.gd ?gereguPs_gt12.gd.mw:0) +
+            (gereguPs_gt13.gd ?gereguPs_gt13.gd.mw:0))
+                ).toFixed(2), kv: (gereguPs_r2j.gd ?gereguPs_r2j.gd.V:0)},
         'AZURA-EDO IPP (GAS)' : {mw: Number(
-            -((ihovborNippPs_ohl1.gd.mw?ihovborNippPs_ohl1.gd.mw:0) + (ihovborNippPs_ohl2.gd.mw?ihovborNippPs_ohl2.gd.mw:0))
-        ).toFixed(2), kv: (ihovborNippPs_ohl1.gd.V?ihovborNippPs_ohl1.gd.V:0)},
+            -((ihovborNippPs_ohl1.gd ?ihovborNippPs_ohl1.gd.mw:0) + (ihovborNippPs_ohl2.gd ?ihovborNippPs_ohl2.gd.mw:0))
+        ).toFixed(2), kv: (ihovborNippPs_ohl1.gd ?ihovborNippPs_ohl1.gd.V:0)},
         'TRANS-AMADI (GAS)' : {mw: Number(
-            phMain_m21p.td.mw?phMain_m21p.td.mw:0
-            ).toFixed(2), kv: phMain_m21p.td.V?phMain_m21p.td.V:0},
+            phMain_m21p.td ?phMain_m21p.td.mw:0
+            ).toFixed(2), kv: phMain_m21p.td ?phMain_m21p.td.V:0},
         'IBOM POWER (GAS)' : {mw: Number(
-            -((eket_e21m.td.mw?eket_e21m.td.mw:0) + (eket_e22m.td.mw?eket_e22m.td.mw:0)) - (ekim_ek1m.td.mw?ekim_ek1m.td.mw:0)
-        ).toFixed(2), kv: (eket_e21m.td.V?eket_e21m.td.V:0)},
+            -((eket_e21m.td ?eket_e21m.td.mw:0) + (eket_e22m.td ?eket_e22m.td.mw:0)) - (ekim_ek1m.td ?ekim_ek1m.td.mw:0)
+        ).toFixed(2), kv: (eket_e21m.td ?eket_e21m.td.V:0)},
         'GBARAIN NIPP (GAS)' : {mw: Number(
-            (gbarain_st1.gd.mw?gbarain_st1.gd.mw:0) + (gbarain_st2.gd.mw?gbarain_st2.gd.mw:0)
-            ).toFixed(2), kv: (gbarain_st1.gd.V?gbarain_st1.gd.V:0)},
+            (gbarain_st1.gd ?gbarain_st1.gd.mw:0) + (gbarain_st2.gd ?gbarain_st2.gd.mw:0)
+            ).toFixed(2), kv: (gbarain_st1.gd ?gbarain_st1.gd.V:0)},
         'OLORUNSOGO (GAS)' : {mw: Number(
-            (olorunsogo1_tr1.gd.mw?olorunsogo1_tr1.gd.mw:0) + (olorunsogo1_tr2.gd.mw?olorunsogo1_tr2.gd.mw:0) + 
-            (olorunsogoPhase1Gs_tr3.gd.mw?olorunsogoPhase1Gs_tr3.gd.mw:0) + (olorunsogoPhase1Gs_tr4.gd.mw?olorunsogoPhase1Gs_tr4.gd.mw:0)
-                ).toFixed(2), kv: (olorunsogo1_tr1.gd.V?olorunsogo1_tr1.gd.V:0)},
+            (olorunsogo1_tr1.gd ?olorunsogo1_tr1.gd.mw:0) + (olorunsogo1_tr2.gd ?olorunsogo1_tr2.gd.mw:0) + 
+            (olorunsogoPhase1Gs_tr3.gd ?olorunsogoPhase1Gs_tr3.gd.mw:0) + (olorunsogoPhase1Gs_tr4.gd ?olorunsogoPhase1Gs_tr4.gd.mw:0)
+                ).toFixed(2), kv: (olorunsogo1_tr1.gd ?olorunsogo1_tr1.gd.V:0)},
         'DADINKOWA G.S (HYDRO)' : {mw: Number(
-            (dadinKowaGs_w21b.td.mw?dadinKowaGs_w21b.td.mw:0) + (dadinKowaGs_w23e.td.mw?dadinKowaGs_w23e.td.mw:0)
-            ).toFixed(2), kv: dadinKowaGs_w21b.td.V?dadinKowaGs_w21b.td.V:0},
+            (dadinKowaGs_w21b.td ?dadinKowaGs_w21b.td.mw:0) + (dadinKowaGs_w23e.td ?dadinKowaGs_w23e.td.mw:0)
+            ).toFixed(2), kv: dadinKowaGs_w21b.td ?dadinKowaGs_w21b.td.V:0},
     };
     return station_array;
 }
