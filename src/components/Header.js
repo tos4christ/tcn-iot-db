daea7dbfdcda650fad9b4de7303853c286fdd5b8
tos4christ <gnztrades@gmail.com>
2022-09-04 10:40:28 +0100

    re-style headers

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ class Header extends React.Component {
                             <NavLink className='nav-link' to='/'>Developer</NavLink>
                         </li>
                         <li className='nav-item'>
-                            <NavLink className='nav-link' to='/'>Products</NavLink>
+                            <NavLink className='nav-link' to='/'>IOT News</NavLink>
                         </li>
                         <li className='nav-item'>
                             <NavLink className='nav-link' to='/'>Tour</NavLink>
