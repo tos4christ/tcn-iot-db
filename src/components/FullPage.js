4a97613b9167b153aacb4ad5ce9bef53735f9668
tos4christ <gnztrades@gmail.com>
2022-12-27 07:29:52 +0100

    adjust frequency display

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -135,8 +135,7 @@ import get_stations from "./stations_adder";
       <div className="ncc-menu">
         <div className="ncc-menu-list">
           <div className="ncc-display-div">
-            <h1 className="text-danger"> Frequency:  { this.state.frequency }</h1>
-            <h2 className="text-danger">IoT POWER STATIONS TABLE</h2>
+            <h2 className="text-danger">IoT POWER STATIONS TABLE  -- Frequency:  { this.state.frequency }Hz</h2>
             <table className="ncc-tg">
               <thead>
                 <tr>
