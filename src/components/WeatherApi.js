6d47c2b9ac519832213c09e7468069374f33a5a0
tos4christ <gnztrades@gmail.com>
2023-02-18 20:15:58 +0100

    added cors to the weather app

--- src/components/WeatherApi.js
+++ src/components/WeatherApi.js
@@ -38,6 +38,9 @@ function WeatherApi({tickets}) {
     mode: "cors",
     headers: {
         "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "DELETE, POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested-With"
     },
     })
     .then(res => res.json())
