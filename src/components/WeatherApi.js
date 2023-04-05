ec1d961fa004e560918263477fc7a998622ca6c8
tos4christ <gnztrades@gmail.com>
2023-04-05 16:17:39 +0100

    restructure web socket data in conformity to the backend

--- src/components/WeatherApi.js
+++ src/components/WeatherApi.js
@@ -13,8 +13,12 @@ class WeatherApi extends React.Component {
             cards_transmission: false,
             rows_transmission: false,
             stations: [],
-            weather_stations_generation: [],
-            weather_stations_transmission: [],
+            current_weather_stations_generation: [],
+            current_weather_stations_transmission: [],
+            hourly3_weather_stations_generation: [],
+            hourly3_weather_stations_transmission: [],
+            daily_weather_stations_generation: [],
+            daily_weather_stations_transmission: [],
             station: [],
             stations_promise_array: [],
             current_display: false,
@@ -38,26 +42,54 @@ class WeatherApi extends React.Component {
     }
     componentDidMount() {
         if(this.props.history.location.pathname === "/nccweather") {
-            socket.on("client_message_weather", data => {
+            socket.on("client_message_weather_current", data => {
               const { message } = data;
               const parsedStation = JSON.parse(message);
               const weather_stations_generation = parsedStation.filter(station => station.type === 'GENERATION');
               const weather_stations_transmission = parsedStation.filter(station => station.type === 'TRANSMISSION');
               const returnObject = {}
               this.setState(prevState => {
-                prevState["weather_stations_generations"] = weather_stations_generation;
-                prevState["weather_stations_transmission"] = weather_stations_transmission;
-                returnObject["weather_stations_generations"] = prevState["weather_stations_generations"];
-                returnObject["weather_stations_transmission"] = prevState["weather_stations_transmission"]
+                prevState["current_weather_stations_generation"] = weather_stations_generation;
+                prevState["current_weather_stations_transmission"] = weather_stations_transmission;
+                returnObject["current_weather_stations_generation"] = prevState["current_weather_stations_generation"];
+                returnObject["current_weather_stations_transmission"] = prevState["current_weather_stations_transmission"]
                 return returnObject;
               })
             });
+            socket.on("client_message_weather_hourly3", data => {
+                const { message } = data;
+                const parsedStation = JSON.parse(message);
+                const weather_stations_generation = parsedStation.filter(station => station.type === 'GENERATION');
+                const weather_stations_transmission = parsedStation.filter(station => station.type === 'TRANSMISSION');
+                const returnObject = {}
+                this.setState(prevState => {
+                  prevState["hourly3_weather_stations_generation"] = weather_stations_generation;
+                  prevState["hourly3_weather_stations_transmission"] = weather_stations_transmission;
+                  returnObject["hourly3_weather_stations_generation"] = prevState["hourly3_weather_stations_generation"];
+                  returnObject["hourly3_weather_stations_transmission"] = prevState["hourly3_weather_stations_transmission"]
+                  return returnObject;
+                })
+              });
+              socket.on("client_message_weather_daily", data => {
+                const { message } = data;
+                const parsedStation = JSON.parse(message);
+                const weather_stations_generation = parsedStation.filter(station => station.type === 'GENERATION');
+                const weather_stations_transmission = parsedStation.filter(station => station.type === 'TRANSMISSION');
+                const returnObject = {}
+                this.setState(prevState => {
+                  prevState["daily_weather_stations_generation"] = weather_stations_generation;
+                  prevState["daily_weather_stations_transmission"] = weather_stations_transmission;
+                  returnObject["daily_weather_stations_generation"] = prevState["daily_weather_stations_generation"];
+                  returnObject["daily_weather_stations_transmission"] = prevState["daily_weather_stations_transmission"]
+                  return returnObject;
+                })
+              });
           }
     };
 
   render() {
-    console.log(this.state.weather_stations_generation, " the generation data");
-    console.log(this.state.weather_stations_transmission, " the transmission data");
+    console.log(this.state.daily_weather_stations_generation, " the generation data daily");
+    console.log(this.state.daily_weather_stations_transmission, " the transmission data daily");
     return <>
         <div className='container-fluid'>
             <div className='row dashboard'>
