f9df49ce70e3f13ffdf34c4f6db679dc6350323b
Chukwuemeka <mogdemdaz@gmail.com>
2023-05-28 13:43:27 +0100

    Removed Date label from the DateTime component

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from "react";
 
 const dateStyle = {
-    color: "white",
+    color: "#DC4C64",
     fontWeight: "bold"
  }
 
@@ -16,7 +16,7 @@ const DateTime = () => {
 
     return(
         <div style={dateStyle}>
-            <p>Date: {date.toDateString()} --- {date.toLocaleTimeString()}</p>
+            <p>{date.toDateString()} --- {date.toLocaleTimeString()}</p>
         </div>
     )
 }
