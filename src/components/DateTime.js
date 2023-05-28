a3f8c0aa94f4b0030a98df6b30070cbcd253f415
Chukwuemeka <mogdemdaz@gmail.com>
2023-05-28 20:14:01 +0100

    Removed the 'return function cleanup' function from the datetime function

--- src/components/DateTime.js
+++ src/components/DateTime.js
@@ -9,9 +9,6 @@ const DateTime = () => {
     var [date, setDate] = useState(new Date());
     useEffect(() => {
         var timer = setInterval(() => setDate(new Date()), 1000);
-        return function cleanup(){
-            clearInterval(timer);
-        }
     });
 
     return(
