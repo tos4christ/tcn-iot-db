f5dbe2f51a3487bf670b7dddf7d95cb9143e7c6b
tos4christ <gnztrades@gmail.com>
2024-06-28 18:54:08 +0100

    log the client data for error checks

--- src/components/FrequencyTest.js
+++ src/components/FrequencyTest.js
@@ -14,6 +14,7 @@ import socket from "./utility/socketIO";
       socket.on("frequency001", data => {
         const { message } = data;
         const parsedMessage = JSON.parse(message);
+        console.log("the message  ", message, "  parsedMessage  ", parsedMessage );
         const returnObject = {}
         this.setState(prevState => {
           prevState["frequency"] = parsedMessage;
