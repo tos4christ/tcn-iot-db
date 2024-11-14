999690e52191f26c1a7e54057b2aabe55d55b2e5
tos4christ <gnztrades@gmail.com>
2024-11-14 09:56:26 +0100

    protect main route

--- src/components/FullPage.js
+++ src/components/FullPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { withRouter } from 'react-router-dom';
+import { withRouter, Redirect } from 'react-router-dom';
 import socket from "./utility/socketIO";
 import get_stations from "./stations_adder";
 import DateTime from "./DateTime";
