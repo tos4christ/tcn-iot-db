8fa5a3d8c62ee34c06588a68f94c7827c487bf3c
tos4christ <gnztrades@gmail.com>
2025-03-21 12:53:15 +0100

    fix bug

--- src/components/BILATERAL_Page.js
+++ src/components/BILATERAL_Page.js
@@ -246,14 +246,14 @@ import Modal from "./Modal";
                   <td>PULKIT ALLOY & STEEL IKORODU</td>
                   <td>{this.checkConnection2(this.state.pulkitSteel.server_time)}</td>
                   <td>{Math.abs(pulkitSteel?.mw ? pulkitSteel.mw : 0)}</td>
-                  <td>{pulkitSteel.v}</td>
+                  <td>{pulkitSteel?.v ? pulkitSteel.v : 0}</td>
                 </tr>
                 <tr onClick={(e) => { this.setModalTrue(e, ['SUNFLAG IRON & STEEL IKORODU', this.state.sunflag]); }}>
                   <td>3</td>
                   <td>SUNFLAG IRON & STEEL IKORODU</td>
                   <td>{this.checkConnection2(this.state.sunflag.server_time)}</td>
                   <td>{Math.abs(sunflag?.mw ? sunflag.mw : 0)}</td>
-                  <td>{sunflag.v}</td>
+                  <td>{sunflag?.v ? sunflag.v : 0}</td>
                 </tr>
 
                 <tr  onClick={(e) => { this.setModalTrue(e, ['FMPIA', this.state["First Maximum Point Industries Akure"]]); }}>
--- src/components/NDPHC_Page.js
+++ src/components/NDPHC_Page.js
@@ -173,15 +173,15 @@ import Modal from "./Modal";
                   <td>2</td>
                   <td>PULKIT ALLOY & STEEL IKORODU</td>
                   <td>{this.checkConnection2(this.state.pulkitSteel.server_time)}</td>
-                  <td>{Math.abs(pulkitSteel.mw)}</td>
-                  <td>{pulkitSteel.v}</td>
+                  <td>{Math.abs(pulkitSteel?.mw ? pulkitSteel.mw : 0)}</td>
+                  <td>{pulkitSteel?.v ? pulkitSteel.v : 0}</td>
                 </tr>
                 <tr onClick={(e) => { this.setModalTrue(e, ['SUNFLAG IRON & STEEL IKORODU', this.state.sunflag]); }}>
                   <td>3</td>
                   <td>SUNFLAG IRON & STEEL IKORODU</td>
                   <td>{this.checkConnection2(this.state.sunflag.server_time)}</td>
-                  <td>{Math.abs(sunflag.mw)}</td>
-                  <td>{sunflag.v}</td>
+                  <td>{Math.abs(sunflag?.mw ? sunflag.mw : 0)}</td>
+                  <td>{sunflag?.v ? sunflag.v : 0}</td>
                 </tr>
 
                 <tr></tr>
