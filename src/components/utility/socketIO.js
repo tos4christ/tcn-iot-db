339858ec26f8958c3076ab2c1b1c517e74e0d981
tos4christ <gnztrades@gmail.com>
2025-04-20 22:55:54 +0100

    protect the websockets connection from being scrapped by puppeteer or selenium

--- src/components/utility/socketIO.js
+++ src/components/utility/socketIO.js
@@ -2,10 +2,10 @@ import openSocket from 'socket.io-client';
 // start the client socket
 // TCN NAS Server
 const socket = openSocket('https://tcnnas.org', {
-    transports: ['websocket'],
-    secure: true,
-    rejectUnauthorized: true,
-    reconnect: true,
+    // transports: ['websocket'],
+    // secure: true,
+    // rejectUnauthorized: true,
+    // reconnect: true,
     auth: {
         token: localStorage.getItem('token')
     }
