6929ab5757510859c26b0d0a0830ae6aec6e566a
tos4christ <gnztrades@gmail.com>
2025-09-12 19:32:21 +0100

    add tailwindcss properly to the project from using cdn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import './tailwind.css';
 import "./assets/css/bootstrap.min.css";
 import "./assets/css/slicknav.min.css";
 import "./assets/css/icofont.css";
