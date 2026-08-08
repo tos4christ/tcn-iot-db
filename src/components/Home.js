b89cfc0196a4ce721317942dc8debd5b3d4654c6
tos4christ <gnztrades@gmail.com>
2026-08-08 15:10:38 +0100

    update bilateral js for stations not showing

--- src/components/Home.js
+++ src/components/Home.js
@@ -24,7 +24,7 @@ import axios from "axios";
       kamSteel: {},
       ikorodu1: {},
       ikorodu2: {},
-      phoenix: {},
+      pheonix: {},
       sagamu: {},
       pulkitSteel: {},
       africanFoundriesLimited: {},
